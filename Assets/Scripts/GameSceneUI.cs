49fb8465f70a124111e698cfd7bf7e23065b4271
axhiggin <93854415+AidanHig@users.noreply.github.com>
2024-03-12 17:52:19 -0700

    changed health bar to slider

--- Assets/Scripts/GameSceneUI.cs
+++ Assets/Scripts/GameSceneUI.cs
@@ -2,27 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSceneUI : MonoBehaviour
 {
     private Canvas canvas;
     [SerializeField] GameObject player;
-    private TextMeshProUGUI roundCount, timeRemaining, playerLives, materialCount;
+    private TextMeshProUGUI roundCount, timeRemaining, materialCount;
+    private Slider playerLives;
 
     void Start()
     {
         canvas = GetComponent<Canvas>();
         roundCount = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
         timeRemaining = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
-        playerLives = transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+        playerLives = transform.GetChild(2).GetComponent<Slider>();
+        playerLives.maxValue = GameManager.DEFAULT_LIVES;
         materialCount = transform.GetChild(3).GetComponent<TextMeshProUGUI>();
     }
 
     private void Update()
     {
         materialCount.text = "Materials:       x" + player.GetComponent<PlayerBuild>().materialCount;
-        playerLives.text = "Lives: " + GameManager.Instance.getLivesRemaining();
+        playerLives.value = GameManager.Instance.getLivesRemaining();
         roundCount.text = "Round: " + GameManager.Instance.getLevelCount();
-        timeRemaining.text = "Lives: " + GameManager.Instance.getCurrentTimeRemaining();
+        timeRemaining.text = "Time: " + GameManager.Instance.getCurrentTimeRemaining();
     }
 }
